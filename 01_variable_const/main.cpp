331cedf4d7594d739da7e205bed17a74cabc21f5
praveen <palpraveen3221@gmail.com>
2025-05-03 07:18:48 +0530

    variable and constant

---
+++ 01_variable_const/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    int score;
+    score = 110;
+
+    const int uid = 232323;
+
+    int praveenBalance = 500;
+    praveenBalance = 1000;
+
+    // uid = 1223;
+    cout << "Welcome to Our with cpp 1" << endl ;
+
+    
+    cout << "Welcome to Our with cpp 2" << endl ;
+    cout << "Welcome to Our with cpp 3" << endl ;
+
+    return 0;
+}
